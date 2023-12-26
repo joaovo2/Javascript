f6376fc514e56f08f6ecb4290e889d8189debf1c
João Vitor <joaovitoroliveirasoarespereira@gmail.com>
2023-12-25 23:56:09 -0300

    Conceitos sobre variáveis em javascript

---
+++ Variables/main.js
@@ -0,0 +1,36 @@
+/* 
+Para se declarar uma variável em javascript 
+utiliza-se uma das palavras reservadas para este
+fim: var, let, const
+*/
+
+/*
+var é a palavra-chave mais antiga e é o padrão em 
+JavaScript até a versão ES6. 
+Variáveis declaradas com var são escopoadas de 
+forma lexical, o que significa que seu escopo é 
+determinado pela posição na qual são declaradas. 
+Variáveis var também podem ser reatribuídas 
+quantas vezes quisermos.
+*/
+var numeroPrincipal = 34;
+
+/* 
+let é uma palavra-chave introduzida na versão ES6. 
+Variáveis declaradas com let são escopoadas de 
+forma dinâmica, o que significa que seu escopo é 
+determinado pelo bloco de código no qual são 
+declaradas. Variáveis let também podem ser 
+reatribuídas, mas não podem ser declaradas 
+novamente dentro do mesmo escopo.
+*/
+let valorDigitado = 122;
+
+/*
+const é outra palavra-chave introduzida na versão 
+ES6. Variáveis declaradas com const são 
+constantes e não podem ser reatribuídas. 
+Variáveis const também são escopoadas de forma 
+dinâmica.
+*/
+const nome = "João Vitor";
